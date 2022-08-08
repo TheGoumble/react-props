babfe4df32d313ebb349deb7ab4206bcd07d5c09
Javier Vargas <3231javiervargas@gmail.com>
2022-08-08 16:13:06 -0400

    added coffee component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer"
 import Main from "./components/Main"
 import Menubar from "./components/Menubar"
 import "./App.css"
+import Coffee from "./components/Coffee"
 
 function App() {
   const [name, setName] = useState("Mason")
@@ -11,6 +12,7 @@ function App() {
     <>
       <Menubar name={name} setName = {setName}/>
       <Main name={name} />
+      <Coffee/>
       <Footer />
     </>
   )
