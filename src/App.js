2d27302e68edc29b0ca2d6ece523f20b4c8e8186
Javier Vargas <3231javiervargas@gmail.com>
2022-08-08 15:00:14 -0400

    fake login and logout

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Menubar />
+      <Menubar name={name} setName = {setName}/>
       <Main name={name} />
       <Footer />
     </>
--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-export default function Menubar(){
-    return(
-        <nav>
-            Menubar
-        </nav>
-    )
+import React from "react"
+export default function Menubar({name, setName}) {
+  return (
+    <nav style={{display: "flex", justifyContent: "space-between"}}>
+      <span>Menubar</span>
+      {/* REMEBER THIS  */}
+      {name ? <button onClick={()=>{setName(null)}}>LogOut</button>:<button onClick={()=>{setName("Javier")}}>Login</button>}
+    </nav>
+  )
 }
