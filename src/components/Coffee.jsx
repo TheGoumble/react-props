7f2762f9ed130539b87cf8919c247d77e68c45da
Javier Vargas <3231javiervargas@gmail.com>
2022-08-08 16:53:39 -0400

    rewrote coffee with async await

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -5,23 +5,35 @@ export default function Coffee() {
   const getCoffee = (temperature) => {
     // fetch coffee api
     fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-    .then(results => results.json())
-    .then(data => setCoffeeList(data))
-    .catch(err => alert(err))
-    // results -> setCoffeeList
+      .then((results) => results.json())
+      .then((data) => setCoffeeList(data))
+      .catch((err) => alert(err))
   }
+
+  const getCoffeeSync = async (temperature) => {
+    try {
+    const results = await fetch(
+      `https://api.sampleapis.com/coffee/${temperature}`);
+      const data = await results.json();
+      setCoffeeList(data);
+    }
+    catch(err){
+        alert(err);
+    }
+  }
+
   return (
     <>
       <button
         onClick={() => {
-          getCoffee("hot")
+          getCoffeeSync("hot")
         }}
       >
         HOT
       </button>
       <button
         onClick={() => {
-          getCoffee("iced")
+          getCoffeeSync("iced")
         }}
       >
         ICED
